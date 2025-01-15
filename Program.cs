e80f0686d499e2ab5def914708b0575132ee4745
André Hessels <andrehessels@outlook.com>
2025-01-15 22:24:36 +0100

    Aanpassen bestandsnaam naar AccommodationService.cs

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ namespace BlazorApp
             builder.Services.AddDbContext<BookingAppContext>(options =>
                 options.UseSqlServer("Server=HP-ANDRÉ;Database=booking-app;Trusted_Connection=True;TrustServerCertificate=True;"));
 
-            builder.Services.AddScoped<AccommodationService>();
+            builder.Services.AddScoped<BookingService>();
             builder.Services.AddScoped<CustomerService>();
 
             var app = builder.Build();
